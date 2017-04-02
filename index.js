3cb0db0da34b7059587556726d338f44106f93d9
Dhanush123 <dhanush.patel@ymail.com>
2017-04-02 07:17:52 -0700

    salads
    asdasd

--- index.js
+++ index.js
@@ -335,7 +335,8 @@ dialog.matches("ivvAnalyze",[
       if (error) throw new Error(error);
       body = JSON.parse(body);
       console.log(body);
-      var hRDate = body.resultMap.RETURNS[0].highReturnDate;
+      var hRDate = " ";
+      hRDate = body.resultMap.RETURNS[0].highReturnDate;
       hRDate = hRDate.substring(4,6) + "/" + hRDate.substring(6,8) + "/" + hRDate.substring(0, 4);
       var rMap = body.resultMap.RETURNS[0].returnsMap;
       var rMLength = Object.keys(rMap).length;
