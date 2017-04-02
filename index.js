5d644b1da541448dfe54ca5e3816dded986a9b06
Dhanush123 <dhanush.patel@ymail.com>
2017-04-02 00:14:03 -0700

    dfsdf
    sdfsd

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var request = require('request');
 var fs = require('fs');
-var firebase = require("firebase");
+var cloudinary = require('cloudinary');
 
-var config = {
-  apiKey: "AIzaSyAeE2nmOHgdjCn50AWMztNIHu4hC4KJ8Gw",
-    authDomain: "la-hacks-2017-4d868.firebaseapp.com",
-    databaseURL: "https://la-hacks-2017-4d868.firebaseio.com",
-    projectId: "la-hacks-2017-4d868",
-    storageBucket: "la-hacks-2017-4d868.appspot.com",
-    messagingSenderId: "474072545"
-};
-firebase.initializeApp(config);
-var storageRef = firebase.storage().ref();
-var imgyRef = storageRef.child('imgy.jpg');
+cloudinary.config({
+  cloud_name: 'octabytes',
+  api_key: '393134871951925',
+  api_secret: 'fegjRet5hCx2RxgUk3NuYpuXd-s'
+});
 
 var botConnectorOptions = {
   appId: "c8431942-19ab-42a1-b6c1-457eb8398648",
@@ -58,51 +52,31 @@ dialog.matches('Greeting',[
       download(results.response.contentUrl, 'imgy.png', function(){
         console.log('done');
 
-                // function to encode file data to base64 encoded string
-        function base64_encode(file) {
-            // read binary data
-            var bitmap = fs.readFileSync(file);
-            // convert binary data to base64 encoded string
-            return new Buffer(bitmap).toString('base64');
-        }
+        cloudinary.uploader.upload('imgy.png', function(result) { console.log(result) },
+                                   { public_id: "imgy" });
 
-        // function to create file from base64 encoded string
-        function base64_decode(base64str, file) {
-            // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-            var bitmap = new Buffer(base64str, 'base64');
-            // write buffer to file
-            fs.writeFileSync(file, bitmap);
-            console.log('******** File created from base64 encoded string ********');
-        }
 
-        // convert image to base64 encoded string
-        var base64str = base64_encode('imgy.jpg');
-        console.log("base64",base64str);
 
-        storageRef.putString(base64str, 'base64').then(function(snapshot) {
-          console.log('Uploaded a base64 string!');
+                  var options = { method: 'POST',
+                    url: 'https://vision.googleapis.com/v1/images:annotate',
+                    qs: { key: 'AIzaSyCVP_E8hjQHzd4nRAC9wrnFfpzkvOuypl4' },
+                    headers:
+                     {
+                       accept: 'application/json',
+                       'content-type': 'application/json' },
+                    body:
+                     { requests:
+                        [ { features: [ { type: 'LOGO_DETECTION', maxResults: 3 } ],
+                            image: { source: { imageUri: "http://res.cloudinary.com/octabytes/image/upload/v1491116301/imgy.jpg" } } } ] },
+                    json: true };
+                  //results.response.contentUrl
+                  request(options, function (error, response, body) {
+                    if (error) throw new Error("GOOGLE ERROR: " + error);
 
-          var options = { method: 'POST',
-            url: 'https://vision.googleapis.com/v1/images:annotate',
-            qs: { key: 'AIzaSyCVP_E8hjQHzd4nRAC9wrnFfpzkvOuypl4' },
-            headers:
-             {
-               accept: 'application/json',
-               'content-type': 'application/json' },
-            body:
-             { requests:
-                [ { features: [ { type: 'LOGO_DETECTION', maxResults: 3 } ],
-                    image: { source: { imageUri: storageRef.child('imgy.jpg').getDownloadURL() } } } ] },
-            json: true };
-          //results.response.contentUrl
-          request(options, function (error, response, body) {
-            if (error) throw new Error("GOOGLE ERROR: " + error);
-
-            console.log("GOOGLE BODY1: "+JSON.stringify(body));
-            console.log("GOOGLE BODY2: "+JSON.stringify(body.responses));
-            // console.log("GOOGLE BODY3: "+body.logoAnnotations.description);
-          });
-        });
+                    console.log("GOOGLE BODY1: "+JSON.stringify(body));
+                    console.log("GOOGLE BODY2: "+JSON.stringify(body.responses));
+                    // console.log("GOOGLE BODY3: "+body.logoAnnotations.description);
+                  });
       });
     }
 ]);
