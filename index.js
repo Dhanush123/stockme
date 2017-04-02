a127185e8b3b62b734e64ea109f61efae0dea9cc
Dhanush123 <dhanush.patel@ymail.com>
2017-04-02 00:17:25 -0700

    dsfsdf
    pdf

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ dialog.matches('Greeting',[
 
       var download = function(uri, filename, callback){
         request.head(uri, function(err, res, body){
-          console.log('content-type:', res.headers['content-type']);
-          console.log('content-length:', res.headers['content-length']);
+          // console.log('content-type:', res.headers['content-type']);
+          // console.log('content-length:', res.headers['content-length']);
 
           request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
         });
