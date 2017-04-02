695c43532816543e1adaa37824361aa76673c4d9
Dhanush123 <dhanush.patel@ymail.com>
2017-04-01 18:35:18 -0700

    dasd
    asd

--- index.js
+++ index.js
@@ -15,22 +15,20 @@ server.listen((process.env.PORT || 8000), function () {
   console.log("Server listening");
 });
 
-bot.dialog('/', [
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/464a8df5-febf-410f-b18f-b3d9d7239013?subscription-key=c83e960c6e4a4807902c0e4c68148196&timezoneOffset=0.0&verbose=true&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+dialog.matches('Greeting',[
    function (session) {
         builder.Prompts.text(session, "Hello... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?");
+        builder.Prompts.attachment(session, "Hi " + results.response + "What picture would you like me to analyze for a logo?");
     },
     function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name +
-                     " you've been programming for " + session.userData.coding +
-                     " years and use " + session.userData.language + ".");
+      console.log(results);
     }
 ]);
